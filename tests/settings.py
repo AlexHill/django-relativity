9902a6b6f717d411b850821e4b5158bfa04c8eef
Alex Hill <alex@hill.net.au>
2018-09-15 01:18:44 +0800

    Simplify test structure - pilfered from django-reverse-unique

--- tests/settings.py
+++ tests/settings.py
@@ -1,10 +1,15 @@
 
-DATABASES = {"default": {"ENGINE": "django.db.backends.sqlite3", "NAME": "test.db"}}
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+    },
+}
 
-INSTALLED_APPS = ["testapp"]
+INSTALLED_APPS = ["tests"]
 
 SECRET_KEY = "test_secret_key"
 
-TEST_NON_SERIALIZED_APPS = ["testapp"]
+TEST_NON_SERIALIZED_APPS = ["tests"]
 
 DEBUG = True
--- tests/tests.py
+++ tests/tests.py
@@ -2,7 +2,7 @@ from __future__ import unicode_literals
 
 from django.test import TestCase
 
-from testapp.models import (
+from .models import (
     CartItem,
     Categorised,
     Category,
