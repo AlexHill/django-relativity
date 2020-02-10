3f11cfb84cdcc7ce108ac6ec5f1a96b3b37a41f8
Alex Hill <alexh@signiq.com>
2020-02-10 10:27:19 +0800

    Fix migrations when a model with a Relationship is referenced by a FK

---
+++ relativity/__init__.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import django.db.migrations.operations.fields
+
+
+def is_referenced_by_foreign_key(state, model_name_lower, field, field_name):
+    """
+    This is a hack to work around migration problems when using a Relationship
+    field, which has f.to_fields == []. The change is simply to swap the
+    operands of an and, so should have no effect on behaviour.
+    """
+    for state_app_label, state_model in state.models:
+        for _, f in state.models[state_app_label, state_model].fields:
+            if (f.related_model and
+                    '%s.%s' % (state_app_label, model_name_lower) == f.related_model.lower() and
+                    hasattr(f, 'to_fields')):
+                # Code change is here - the clauses of the and in the parens are switched
+                # if (f.to_fields[0] is None and field.primary_key) or field_name in f.to_fields:
+                if (field.primary_key and f.to_fields[0] is None) or field_name in f.to_fields:
+                    return True
+    return False
+
+
+django.db.migrations.operations.fields.is_referenced_by_foreign_key = is_referenced_by_foreign_key
