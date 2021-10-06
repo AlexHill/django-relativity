3fcf7116f197e76fb2254f6c8d94b27ec31102b9
Alex Hill <alex@hill.net.au>
2021-10-06 12:30:27 +0800

    Exclude tests from built package

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/alexhill/django-relativity',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(exclude=['tests']),
     install_requires=['django>=1.11'],
     classifiers=[
         'Development Status :: 4 - Beta',
